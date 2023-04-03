663fc21cac8dd5df128ecc46dbc6ba1d27616675
david <davmwa90@gmail.com>
2023-04-03 19:26:11 +0300

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { useState } from "react";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Footer from "./pages/Footer";
 import Login from "./pages/Login";
 import Navbar from "./pages/Navbar";
@@ -24,6 +25,7 @@ function App() {
           {/* <Route path ="/" element={<Landing />}  /> */}
           <Route path ="signup" element = {<Signup/>} />
           <Route path ="login" element = {<Login isLoggedIn = {isLoggedIn} setIsLoggedIn = {setIsLoggedIn}/>} />
+          <Route path ="contact" element = {<Contact/>} />
           <Route path="services" element ={<Services />}/>
           <Route path="about" element = {<About />}/>
           <Route path="reviews" element ={<Reviews />}/>
