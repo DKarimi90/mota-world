0c47ac379de5cc915eff94fb863d51b024f86e23
david <davmwa90@gmail.com>
2023-04-03 18:42:44 +0300

    updated Navbar.js

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -6,6 +6,7 @@ function Navbar() {
       <h2>Motor World</h2>
       <NavLink to="/">Home</NavLink> 
       <NavLink to="signup">Signup</NavLink> 
+      <NavLink to="login">Login</NavLink>
       
       {/* <NavLink to="contact">Contact Us</NavLink> */}
     </div>
