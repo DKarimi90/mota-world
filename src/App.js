556ebad1d78650c9a343b1ba3e8eae5506d3b0d9
david <davmwa90@gmail.com>
2023-04-03 19:21:31 +0300

    added files and routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { useState } from "react";
+import Footer from "./pages/Footer";
+import Login from "./pages/Login";
+import Navbar from "./pages/Navbar";
+import Reviews from "./pages/Review";
+import Services from "./pages/Services";
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null)
+  const logIn = () => {
+    setIsLoggedIn(true)
+  }
+  const logOut = () => {
+    setIsLoggedIn(false)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* <Route path ="/" element={<Landing />}  /> */}
+          <Route path ="login" element = {<Login isLoggedIn = {isLoggedIn} setIsLoggedIn = {setIsLoggedIn}/>} />
+          <Route path="services" element ={<Services />}/>
+          <Route path="reviews" element ={<Reviews />}/>
+         
+        </Routes>
+        <Footer />
+      </Router>
+  
     </div>
   );
 }
